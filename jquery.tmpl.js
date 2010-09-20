c6f2f73f264e4e861d917ab5d854bce488feb8e6
Dave Taylor <dave@the-taylors.org>
2010-09-20 08:20:04 -0700

    added item index as first argument for external function

--- jquery.tmpl.js
+++ jquery.tmpl.js
@@ -88,7 +88,7 @@
 			},
 			"=": {
 				_default: [ "this" ],
-				prefix: "_.push($.encode(typeof $1==='function'?$1.call(this):$1));"
+				prefix: "_.push($.encode(typeof $1==='function'?$1.call(this,$i):$1));"
 			}
 		},
 
